77680d851ac7abd1ab8ce3af80b743ebc99ca05a
slbin-park <smpts00@naver.com>
2021-10-07 23:11:50 +0900

    change ham scss

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Calendar from './page/calendar/calendar'
 import './assets/css/ham.scss'
 
 const App = () => {
+  const [side_bar, setside_bar] = useState(false);
 
   return (
     <div className="App">
-      <div className="ham">
+      <div className={side_bar == false ? 'ham' : 'ham_click'}>
         <h2>사이드바</h2>
+        <div class="three col">
+          <div class="hamburger" id="hamburger-3">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </div>
+        </div>
       </div>
       <Route path="/" component={Calendar}></Route>
     </div>
